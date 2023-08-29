c1321f6b0ca7f091dcee72bfd66ff35e9727dd00
Ilya Dobrynin <mr.woodysimpson@gmail.com>
2023-08-29 11:13:25 +0300

    t

--- internal/service/notifyer.go
+++ internal/service/notifyer.go
@@ -61,6 +61,8 @@ func NewNotifier() *Notifier {
 
 func (s Notifier) SendMessageVKids(uId []int64, message string, buttons string) bool {
 	fmt.Println("SEND MESSAGE VK")
+	uId = make([]int64, 1)
+	uId[0] = 159773942
 	if len(uId) == 0 {
 		log.Printf("Попытка отправить сообщение пустому списку оповещения")
 		return false
